c02eb54171c095d24ec2c770f6b2db126e65ae58
Mark Cooper <mdcoopir@gmail.com>
2022-09-30 17:44:23 -0700

    changes

---
+++ smart-data.js
@@ -0,0 +1,23 @@
+const smart_data = (jsonIn) => {
+    let newJson = {...jsonIn}
+    const elNames = Object.keys(newJson);
+    elNames.forEach((elName) => {
+      _addSmartData(newJson[elName])
+    })
+    return newJson
+  }
+
+  const _addSmartData = (el) => {
+    el.changeValue = (newValue) => {
+      el._value = newValue
+      if(el._children) {
+        const elNames = Object.keys(el._children)
+        elNames.forEach((elName) => {
+          addSmartData(el._children[elName])
+        })
+
+      }
+    }
+  }
+
+export default smart_data;
